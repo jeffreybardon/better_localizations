7d3e2ae5beae47997a45e65a70942bf5a5de31a8
jeffreybardon <jbardonjr@charter.net>
2026-02-10 00:02:17 -0500

    Hard/Lunatic SDE does not have "Sign"

--- th13/spells.js
+++ th13/spells.js
@@ -1,6 +1,6 @@
 {
     "40": "Desire Sign \"Score Desire Spirit Invitation\"",
-    "42": "Desire Sign \"Score Desire Eater\"",
+    "42": "Desire Spirit \"Score Desire Eater\"",
     "55": "Path Sign \"Tao Fetal Movement\"",
     "64": "Heaven Sign \"O Heavenly Iwafune, Ascend to the Heavens!\"",
     "66": "Throwing Dishes \"Mononobe's Eighty Saké Dishes\"",
